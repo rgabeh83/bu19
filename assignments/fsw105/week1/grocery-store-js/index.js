da589e4f119796b4b6b5602f73ca1257eb5d2b9d
rgabeh83 <48969895+rgabeh83@users.noreply.github.com>
2019-10-20 12:07:44 -0700

    105 week 1 pp1

---
+++ assignments/fsw105/week1/grocery-store-js/index.js
@@ -0,0 +1,13 @@
+var shopper = {
+    firstName: "Gabe",
+    lastName: "Hernandez",
+    age: 36,
+    isAlive: true,
+    hairColor: "brown",
+    groceryCart: ["bananas", "ice cream", "chocolate syrup"],
+    fullName: function(){
+       console.log(this.firstName + "Roy" + this.lastName)
+}
+}
+
+console.log(shopper)
