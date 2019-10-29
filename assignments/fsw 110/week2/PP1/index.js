0299b1565f0a5f358dfa526c112b92db97c19031
Roy Hernandez <royhernandez@Roys-MacBook-Air.local>
2019-10-28 17:44:12 -0700

    I cannot get the li elements to show up

---
+++ assignments/fsw 110/week2/PP1/index.js
@@ -0,0 +1,19 @@
+
+console.log (document)
+var h1 = document.createElement("h1")
+h1.textContent = "Welcome to My JS Site"
+var body = document.getElementById("body")
+document.body.append(h1)
+console.log (h1, body)
+var newP = document.createElement("p")
+newP.textContent = "All this was created with Javascript"
+document.body.append(newP)
+var newOl = document.createElement("newOl")
+var li1 = document.createElement(li)
+li1.textContent = "Number 1"
+var li2 = document.createElement(li)
+li2.textContent = "Number2"
+var li3 = document.createElement(li)
+li3.textContent = "Number 3"
+document.body.append(newOl)
+newOl.append(li1, li2, li3)
