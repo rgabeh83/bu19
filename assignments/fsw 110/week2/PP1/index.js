3b97042f6a609ad8732fc64130e55a2de50930c8
Roy Hernandez <royhernandez@Roys-MacBook-Air.local>
2019-10-31 15:27:23 -0700

    redo

--- assignments/fsw 110/week2/PP1/index.js
+++ assignments/fsw 110/week2/PP1/index.js
@@ -4,17 +4,16 @@ var h1 = document.createElement("h1")
 h1.textContent = "Welcome to My JS Site"
 var body = document.getElementById("body")
 document.body.append(h1)
-console.log (h1, body)
 var newP = document.createElement("p")
 newP.textContent = "All this was created with Javascript"
 document.body.append(newP)
 var newOl = document.createElement("newOl")
-var li1 = document.createElement(li1)
+var li1 = document.createElement("li")
 li1.textContent = "Number 1"
-var li2 = document.createElement(li2)
+var li2 = document.createElement("li")
 li2.textContent = "Number2"
-var li3 = document.createElement(li3)
+var li3 = document.createElement("li")
 li3.textContent = "Number 3"
 document.body.append(newOl)
-document.append(li, li, li)
-console.log
+document.body.append(li1, li2, li3)
+
